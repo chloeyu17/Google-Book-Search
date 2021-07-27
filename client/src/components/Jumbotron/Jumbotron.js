58b5ac7c2d05094b9664ffcdfdc9919ae5876c32
cyu <chloeyu100@gmail.com>
2021-07-26 23:28:52 -0400

    book search

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div>
+            <div className="jumbotron">
+                <h1 className="display-4 text-center myText">Google Book Search with React</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbotron;
